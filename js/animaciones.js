364fafbf54928465382e1883352b29cdb9febdfe
Jose Plasencia Villarejo <plasenciavillarejo@gmail.com>
2023-06-18 01:15:54 +0200

    a

--- js/animaciones.js
+++ js/animaciones.js
@@ -4,6 +4,12 @@
 
 	var cajaRoja = $('.cajaRoja');
 
+	// Me refresca la tecla y no me la concatena
+	cajaRoja.clearQueue();
+
+	// Utilizamos la librería Tween
+	var cajaAzul = $('.cajaAzul');
+	var tl = new TimelineMax();
 
 	function mover(dir){
 		switch(dir){
@@ -13,21 +19,32 @@
 					// -= Signifcia (A la posición que tenga le restas 50 px)
 					top: "-=50px"
 				},100);
+				/*
+					Parámetros de configuración:
+						1.- Objeto a move - cajaAzul.
+						2.- Retardo en segundos (con 0.xxx se le indica en ms)
+						3.- Hacía donde se va a movr el objeto (y: arriba x: abajo)
+				*/
+				tl.to( cajaAzul, 0.100,{y:"-=50"});
 			break;
 			case "aba":
 				cajaRoja.animate({
 					top: "+=50px"
 				},100);
+
+				tl.to( cajaAzul, 0.100,{y:"+=50"});
 			break;
 			case "izq":
 				cajaRoja.animate({
 					left: "+=50px"
 				},100);
+				tl.to( cajaAzul, 0.100,{x:"+=50"});
 			break;
 			case "der":
 				cajaRoja.animate({
 					left: "-=50px"
 				},100);
+				tl.to( cajaAzul, 0.100,{x:"-=50"});
 			break;
 
 			default:
@@ -35,6 +52,7 @@
 					top: "0px",
 					left: "0px"
 				});
+				tl.to( cajaAzul, 0.45,{x:"0px", y:"0px"});
 		}
 	}
 
